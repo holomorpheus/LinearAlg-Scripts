27afd1628675d5c13cbff43483652285d0d617d8
qgabe <replattus@gmail.com>
2020-09-15 22:33:42 -0700

    Add files via upload

--- 2dLinearMapsVisualization.py
+++ 2dLinearMapsVisualization.py
@@ -24,8 +24,9 @@ cos, sin, pi = np.cos, np.sin, np.pi;
 # Shear Matrix.
 A = np.matrix([[1, 1], [0, 1]]);
 
+
 #------------------------------------
-# Defining 4flowers
+# Aesthetic choices
 
 # How many flowers?
 N = 6;
@@ -36,11 +37,22 @@ L = 8;
 # How many gridpoints?
 anglegridsize = 256;
 
+# Which colormap?
+colormapname = 'viridis'
+
+# Plot horizontal or vertical bounds?
+hbounds = False;
+vbounds = False;
+
+
+#------------------------------------
+# Defining flowers
+
 # List of radii.
 rad = np.array([i for i in range(1,N+1)]);
 
 # Colors for the flowers.
-colormap = cm.get_cmap('viridis', len(rad));
+colormap = cm.get_cmap(colormapname, len(rad));
 colors = colormap(np.linspace(.8,.2,len(rad)));
 
 # Define array of angles
@@ -71,14 +83,23 @@ scope  = rad[-1]+.5;
 # Define figure and axes objects.
 fig, ax = plt.subplots(1,2,figsize=(10, 5))
 
-# Plotting the domain.
+# Domain
 
 # Plot axes
 ax[0].axhline(y=0, color='k', alpha=0.5)
 ax[0].axvline(x=0, color='k',alpha=0.5)
-ax[0].axhline(y=rad[-1], color='k',alpha=0.5)
-ax[0].axhline(y=-rad[-1], color='k',alpha=0.5)
-# Plot flowers
+
+# Plot horizontal bounds.
+if hbounds:
+    ax[0].axhline(y=rad[-1], color='k',alpha=0.5)
+    ax[0].axhline(y=-rad[-1], color='k',alpha=0.5)
+
+# Plot vertical bounds.
+if vbounds:
+    ax[0].axvline(x=rad[-1], color='k',alpha=0.5)
+    ax[0].axvline(x=-rad[-1], color='k',alpha=0.5)
+
+# Plot flowers in the domain.
 for i in range(len(rad)):
     ax[0].plot(xyflowers[i,0,:], xyflowers[i,1,:], color=colors[i], alpha=0.8)
 ax[0].grid(True)
@@ -87,14 +108,23 @@ ax[0].set_xlim([-scope, scope])
 ax[0].set_ylim([-scope, scope])
 ax[0].set_title("Original grid in x-y space")
 
-# Plotting the target space.
+# Target Space
 
-# Plot axes
+# Plot axes.
 ax[1].axhline(y=0, color='k', alpha=0.5)
 ax[1].axvline(x=0, color='k', alpha=0.5)
-ax[1].axhline(y=rad[-1], color='k',alpha=0.5)
-ax[1].axhline(y=-rad[-1], color='k',alpha=0.5)
-# Plot flowers
+
+# Plot horizontal bounds.
+if hbounds:
+    ax[1].axhline(y=rad[-1], color='k',alpha=0.5)
+    ax[1].axhline(y=-rad[-1], color='k',alpha=0.5)
+
+# Plot vertical bounds.
+if vbounds:
+    ax[0].axvline(x=rad[-1], color='k',alpha=0.5)
+    ax[0].axvline(x=-rad[-1], color='k',alpha=0.5)
+
+# Plot flowers in target space.
 for i in range(len(rad)):
     ax[1].plot(uvflowers[i,0,:], uvflowers[i,1,:], color=colors[i], alpha=0.8)
 
